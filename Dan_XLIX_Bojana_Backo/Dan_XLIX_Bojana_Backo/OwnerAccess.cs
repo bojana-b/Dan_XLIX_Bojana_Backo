9e2f78bca9c13fbed85471844fa5ad9bf4c91d8d
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-08-04 13:30:19 +0200

    Add OwnerWindow.xaml, Add AddManager.xaml

---
+++ Dan_XLIX_Bojana_Backo/Dan_XLIX_Bojana_Backo/OwnerAccess.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dan_XLIX_Bojana_Backo
+{
+    public class OwnerAccess
+    {
+        public static string fileOwnerAccess = @"..\..\OwnerAccess.txt";
+
+        /// <summary>
+        /// Function that write to file username and password for the owner
+        /// </summary>
+        public static void AddToFile()
+        {
+            try
+            {
+                File.Delete(fileOwnerAccess);
+                var hash = SecurePasswordHasher.Hash("Vlasnik2020");
+                using(StreamWriter sw = File.CreateText(fileOwnerAccess))
+                {
+                    sw.WriteLine("Vlasnik2020");
+                    sw.WriteLine(hash);
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                MessageBox.Show($"The file was not found: '{e}'");
+            }
+            catch(IOException e)
+            {
+                MessageBox.Show($"The file could not be opened: '{e}'");
+            }
+        }
+    }
+}
---
+++ Dan_XLIX_Bojana_Backo/Dan_XLIX_Bojana_Backo/View/LoginScreen.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dan_XLIX_Bojana_Backo.View
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+            OwnerAccess.AddToFile();
+            //MessageBox.Show("upisano u file");
+        }
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            var hash = SecurePasswordHasher.Hash("Vlasnik2020");
+            try
+            {
+                if (txtUsername.Text.Equals("Vlasnik2020") && SecurePasswordHasher.Verify(txtPassword.Password, hash))
+                {
+                    OwnerWindow dashboard = new OwnerWindow();
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or password is incorrect.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //sqlCon.Close();
+            }
+        }
+    }
+}
